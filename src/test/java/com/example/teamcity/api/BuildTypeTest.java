7c0b5904d925a94472ff7ee91009763e2f578cff
oa <a@ggsel.net>
2025-02-17 01:05:11 +0200

    Two more tests added

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -2,6 +2,8 @@ package com.example.teamcity.api;
 
 import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
+import com.example.teamcity.api.models.Role;
+import com.example.teamcity.api.models.Roles;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
@@ -10,10 +12,10 @@ import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.example.teamcity.api.enums.Endpoint.*;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
-import static io.qameta.allure.Allure.step;
 
 public class BuildTypeTest extends BaseApiTest {
     @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
@@ -49,25 +51,57 @@ public class BuildTypeTest extends BaseApiTest {
 
     @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive", "Roles"})
     public void projectAdminCreatesBuildTypeTest() {
-        step("Create user");
-        step("Create project");
-        step("Grant user PROJECT_ADMIN role in project");
+        String projectId = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
 
-        step("Create buildType for project by user (PROJECT_ADMIN)");
-        step("Check buildType was created successfully");
+        //подумать как тут сократить
+        var user = testData.getUser().toBuilder()
+                .roles(Roles.builder()
+                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + projectId).build()))
+                        .build())
+                .build();
+
+        superUserCheckRequests.getRequest(USERS).create(user);
+
+        var userCheckRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        //последние 3 строки совпадают с первым тестом
+        userCheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
+
+        var createdBuildType = userCheckRequests.<BuildType>getRequest(BUILD_TYPES).read(testData.getBuildType().getId());
+
+        softy.assertEquals(testData.getBuildType().getName(), createdBuildType.getName(), "Build type name is not correct");
     }
 
     @Test(description = "Project admin should not be able to create build type for not their project", groups = {"Negative", "Roles"})
     public void projectAdminCreatesBuildTypeForAnotherUserProjectTest() {
-        step("Create user1");
-        step("Create project1");
-        step("Grant user1 PROJECT_ADMIN role in project1");
+        String project1Id = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
 
-        step("Create user2");
-        step("Create project2");
-        step("Grant user2 PROJECT_ADMIN role in project2");
+        var user1 = testData.getUser().toBuilder()
+                .roles(Roles.builder()
+                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + project1Id).build()))
+                        .build())
+                .build();
 
-        step("Create buildType for project1 by user2");
-        step("Check buildType was not created with forbidden code");
+        superUserCheckRequests.getRequest(USERS).create(user1);
+
+        testData = generate();
+        //дублирование создания проекта и юзера
+        String projectId2 = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
+
+        //подумать как тут сократить аналогично третьему тесту. Также код дублируется с третьим тестом
+        var user2 = testData.getUser().toBuilder()
+                .roles(Roles.builder()
+                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + projectId2).build()))
+                        .build())
+                .build();
+
+        superUserCheckRequests.getRequest(USERS).create(user2);
+
+        var buildType2WithBuildType1Id = testData.getBuildType().toBuilder().project(Project.builder().id(project1Id).build()).build();
+
+        new UncheckedBase(Specifications.authSpec(testData.getUser()), BUILD_TYPES)
+                .create(buildType2WithBuildType1Id)
+                .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN)
+                .body(Matchers.containsString("You do not have enough permissions to edit project with id: " + project1Id + "\nAccess denied. Check the user has enough permissions to perform the operation."));
     }
 }
