c2f9b4486c6d913f04f5ca61c1ba50c1ee90b574
oa <a@ggsel.net>
2025-02-20 23:36:20 +0200

    Completed base homework

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -2,8 +2,6 @@ package com.example.teamcity.api;
 
 import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
-import com.example.teamcity.api.models.Role;
-import com.example.teamcity.api.models.Roles;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.requests.unchecked.UncheckedBase;
 import com.example.teamcity.api.spec.Specifications;
@@ -12,7 +10,6 @@ import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static com.example.teamcity.api.enums.Endpoint.*;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
@@ -53,18 +50,10 @@ public class BuildTypeTest extends BaseApiTest {
     public void projectAdminCreatesBuildTypeTest() {
         String projectId = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
 
-        //подумать как тут сократить
-        var user = testData.getUser().toBuilder()
-                .roles(Roles.builder()
-                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + projectId).build()))
-                        .build())
-                .build();
-
-        superUserCheckRequests.getRequest(USERS).create(user);
+        createProjectAndUser("PROJECT_ADMIN", "p:" + projectId);
 
         var userCheckRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
 
-        //последние 3 строки совпадают с первым тестом
         userCheckRequests.getRequest(BUILD_TYPES).create(testData.getBuildType());
 
         var createdBuildType = userCheckRequests.<BuildType>getRequest(BUILD_TYPES).read(testData.getBuildType().getId());
@@ -76,26 +65,11 @@ public class BuildTypeTest extends BaseApiTest {
     public void projectAdminCreatesBuildTypeForAnotherUserProjectTest() {
         String project1Id = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
 
-        var user1 = testData.getUser().toBuilder()
-                .roles(Roles.builder()
-                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + project1Id).build()))
-                        .build())
-                .build();
-
-        superUserCheckRequests.getRequest(USERS).create(user1);
-
+        createProjectAndUser("PROJECT_ADMIN", "p:" + project1Id);
         testData = generate();
-        //дублирование создания проекта и юзера
-        String projectId2 = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
 
-        //подумать как тут сократить аналогично третьему тесту. Также код дублируется с третьим тестом
-        var user2 = testData.getUser().toBuilder()
-                .roles(Roles.builder()
-                        .role(List.of(Role.builder().roleId("PROJECT_ADMIN").scope("p:" + projectId2).build()))
-                        .build())
-                .build();
-
-        superUserCheckRequests.getRequest(USERS).create(user2);
+        String project2Id = superUserCheckRequests.<Project>getRequest(PROJECTS).create(testData.getProject()).getId();
+        createProjectAndUser("PROJECT_ADMIN", "p:" + project2Id);
 
         var buildType2WithBuildType1Id = testData.getBuildType().toBuilder().project(Project.builder().id(project1Id).build()).build();
 
